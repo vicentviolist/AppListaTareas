f7505d1655bd6c10e6668abb6a95e6679347cca5
vicentviolist <vicent.violist@gmail.com>
2020-12-28 17:48:59 -0600

    MK add

--- quasar.conf.js
+++ quasar.conf.js
@@ -15,6 +15,9 @@ module.exports = function(/* ctx */) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ["app.scss"],
+      build: {
+      vueRouterMode: 'history',
+      publicPath: 'app-extension-qmarkdown'},
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
